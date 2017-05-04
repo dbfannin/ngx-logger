292d082f58d7e2bbcbadac3d274d35ffd77f3f04
davidf <davidf@realtracs.net>
2017-05-04 08:11:14 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SampleComponent } from './sample.component';
+import { SampleDirective } from './sample.directive';
+import { SamplePipe } from './sample.pipe';
+import { SampleService } from './sample.service';
+
+export * from './sample.component';
+export * from './sample.directive';
+export * from './sample.pipe';
+export * from './sample.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    SampleComponent,
+    SampleDirective,
+    SamplePipe
+  ],
+  exports: [
+    SampleComponent,
+    SampleDirective,
+    SamplePipe
+  ]
+})
+export class SampleModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SampleModule,
+      providers: [SampleService]
+    };
+  }
+}
