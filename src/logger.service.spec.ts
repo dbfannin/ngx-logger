492836248af8f4d0c64c5d803d7963048873169c
Pascal Büttiker <pascalbuettiker@bluewin.ch>
2017-11-30 17:51:56 +0100

    added missing jasmine imports to spec

--- src/logger.service.spec.ts
+++ src/logger.service.spec.ts
@@ -2,6 +2,7 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { NGXLogger } from './logger.service';
+import {} from 'jasmine';
 
 describe('NGXLogger', () => {
   beforeEach(() => {
