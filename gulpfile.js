1d5da7499b1675fc2b4de49d7140747487b42593
Pascal Büttiker <IsNull@users.noreply.github.com>
2017-12-08 16:07:00 +0100

    Angular 5 Support (#35)
    * Added npm version badge
    
    * updated dependencys, removed deprecated http module
    
    * ngc api has changed, use array format
    
    * ensure reproducible builds, commit lock file
    
    * added missing jasmine imports to spec
    
    * updated peer dependencies to reflect changes in main package.json

--- gulpfile.js
+++ gulpfile.js
@@ -45,15 +45,10 @@ gulp.task('inline-resources', function () {
  *    compiled modules to the /build folder.
  */
 gulp.task('ngc', function () {
-  return ngc({
-    project: `${tmpFolder}/tsconfig.es5.json`
-  })
-    .then((exitCode) => {
-      if (exitCode === 1) {
-        // This error is caught in the 'compile' task by the runSequence method callback
-        // so that when ngc fails to compile, the whole compile process stops running
-        throw new Error('ngc compilation failed');
-      }
+    return ngc(['-p', `${tmpFolder}/tsconfig.es5.json`], (error) => {
+        if (error) {
+            throw new Error('ngc compilation failed: ' + error);
+        }
     });
 });
 
