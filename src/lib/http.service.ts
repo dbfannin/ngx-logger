e3c52a33826eec588106b32c424cf7ed0e59c7e3
Ankit Sagar <ankit.sagar@accoliteindia.com>
2019-01-22 13:00:12 +0530

    [Added][Support For params and other http options]

--- src/lib/http.service.ts
+++ src/lib/http.service.ts
@@ -1,7 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {NGXLogInterface} from './types/ngx-log.interface';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NGXLogInterface } from './types/ngx-log.interface';
+
 
 
 @Injectable()
@@ -10,14 +11,34 @@ export class NGXLoggerHttpService {
 
   }
 
-  logOnServer(url: string, log: NGXLogInterface, customHeaders: HttpHeaders): Observable<any> {
+  // logOnServer(url: string, log: NGXLogInterface, customHeaders: HttpHeaders, customParams?: HttpParams): Observable<any> {
+  //   const headers = customHeaders || new HttpHeaders();
+  //   const params = customParams || new HttpParams();
+  //   headers.set('Content-Type', 'application/json');
+
+  //   const options = {
+  //     headers: headers,
+  //     params: params
+  //   };
+
+
+  //   return this.http.post(url, log, options);
+  // }
+
+  logOnServer(url: string, log: NGXLogInterface, options: object): Observable<any> {
+    return this.http.post(url, log, options || {});
+  }
+
+
+  setHeaders(customHeaders?: HttpHeaders): HttpHeaders {
     const headers = customHeaders || new HttpHeaders();
     headers.set('Content-Type', 'application/json');
-
-    const options = {
-      headers: headers
-    };
-
-    return this.http.post(url, log, options);
+    return headers;
   }
+
+  setParams(customParams?: HttpParams): HttpParams {
+    const params = customParams || new HttpParams();
+    return params;
+  }
+
 }
