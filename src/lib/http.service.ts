bd799074a7708bca22cea482dfb314ad039ae38d
Ankit Sagar <ankit.sagar@accoliteindia.com>
2019-01-25 19:42:41 +0530

    [Fixed][Review Comments]

--- src/lib/http.service.ts
+++ src/lib/http.service.ts
@@ -7,23 +7,10 @@ import { NGXLogInterface } from './types/ngx-log.interface';
 
 @Injectable()
 export class NGXLoggerHttpService {
-  constructor(private readonly http: HttpClient) {
-
-  }
+  constructor(private readonly http: HttpClient) { }
 
   logOnServer(url: string, log: NGXLogInterface, options: object): Observable<any> {
     return this.http.post(url, log, options || {});
-  }
-
-  setHeaders(customHeaders?: HttpHeaders): HttpHeaders {
-    const headers = customHeaders || new HttpHeaders();
-    headers.set('Content-Type', 'application/json');
-    return headers;
-  }
-
-  setParams(customParams?: HttpParams): HttpParams {
-    const params = customParams || new HttpParams();
-    return params;
   }
 
 }
