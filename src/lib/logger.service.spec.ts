76a5b3d80eb393f1fce1e4e462da0d2496e63141
bmtheo <31310846+bmtheo@users.noreply.github.com>
2021-05-28 22:25:03 +0200

    Fix unit tests
    Following all the rework on the logger, unit tests are now up to date (but not complete)

--- src/lib/logger.service.spec.ts
+++ src/lib/logger.service.spec.ts
@@ -1,69 +1,37 @@
 import { DatePipe } from '@angular/common';
 import { inject, TestBed } from '@angular/core/testing';
+import { NGXLoggerConfigEngineMock } from 'testing/src/lib/config-engine.mock';
+import { NGXLoggerMapperServiceMock } from 'testing/src/lib/mapper.service.mock';
+import { NGXLoggerMetadataServiceMock } from 'testing/src/lib/metadata.service.mock';
+import { NGXLoggerRulesServiceMock } from 'testing/src/lib/rules.service.mock';
+import { NGXLoggerServerServiceMock } from 'testing/src/lib/server.service.mock';
+import { NGXLoggerWriterServiceMock } from 'testing/src/lib/writer.service.mock';
+import { TOKEN_LOGGER_CONFIG } from './config/iconfig';
+import { TOKEN_LOGGER_CONFIG_ENGINE } from './config/iconfig-engine';
 import { NGXLogger } from './logger.service';
-import { NGXLoggerHttpService } from './http.service';
-import { NGXLoggerHttpServiceMock } from '../../testing/src/lib/http.service.mock';
-import { NGXMapperService } from './mapper/mapper-service';
-import { NGXMapperServiceMock } from '../../testing/src/lib/mapper.service.mock';
-import { LoggerConfig } from './types/logger.config';
+import { TOKEN_LOGGER_MAPPER_SERVICE } from './mapper/imapper-service';
+import { TOKEN_LOGGER_METADATA_SERVICE } from './metadata/imetadata-service';
+import { TOKEN_LOGGER_RULES_SERVICE } from './rules/irules-service';
+import { TOKEN_LOGGER_SERVER_SERVICE } from './server/iserver-service';
 import { NgxLoggerLevel } from './types/logger-level.enum';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { LogPosition } from './types/log-position';
-import { of } from 'rxjs';
-import { FormGroup } from '@angular/forms';
+import { TOKEN_LOGGER_WRITER_SERVICE } from './writer/iwriter-service';
 
 describe('NGXLogger', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         NGXLogger,
-        { provide: NGXLoggerHttpService, useClass: NGXLoggerHttpServiceMock },
-        { provide: NGXMapperService, useClass: NGXMapperServiceMock },
-        { provide: LoggerConfig, useValue: { level: NgxLoggerLevel.ERROR } },
+        { provide: TOKEN_LOGGER_CONFIG, useValue: { level: NgxLoggerLevel.ERROR } },
+        { provide: TOKEN_LOGGER_CONFIG_ENGINE, useClass: NGXLoggerConfigEngineMock },
+        { provide: TOKEN_LOGGER_METADATA_SERVICE, useClass: NGXLoggerMetadataServiceMock },
+        { provide: TOKEN_LOGGER_RULES_SERVICE, useClass: NGXLoggerRulesServiceMock },
+        { provide: TOKEN_LOGGER_MAPPER_SERVICE, useClass: NGXLoggerMapperServiceMock },
+        { provide: TOKEN_LOGGER_WRITER_SERVICE, useClass: NGXLoggerWriterServiceMock },
+        { provide: TOKEN_LOGGER_SERVER_SERVICE, useClass: NGXLoggerServerServiceMock },
         DatePipe
       ]
     });
   });
-
-  it('should handle circular structures', inject(
-    [NGXLogger],
-    (logger: NGXLogger) => {
-      const a = {
-        test: 'test'
-      };
-
-      a['a'] = a;
-
-      spyOn(console, 'error');
-      spyOn(console, 'warn');
-
-
-      logger.error('warn', a);
-      logger.error('test', a);
-
-      expect(console.error).toHaveBeenCalled();
-      expect(console.warn).not.toHaveBeenCalled();
-    }
-  ));
-
-  it('should handle complex circular structures', inject(
-    [NGXLogger],
-    (logger: NGXLogger) => {
-      // This structure is not "stringifyable" this make sure anything can be logged
-      // Before that we used JSON.stringify and it was not working
-      const complexStructure = new FormGroup({ sub: new FormGroup({}) });
-
-      spyOn(console, 'error');
-
-      logger.error('error', complexStructure);
-
-      expect(console.error).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), jasmine.anything(), complexStructure);
-
-      logger.error(complexStructure);
-
-      expect(console.error).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), complexStructure);
-    }
-  ));
 
   describe('trace', () => {
     it('should call _log with trace', inject(
@@ -163,45 +131,15 @@ describe('NGXLogger', () => {
         expect(logger.level).toEqual(NgxLoggerLevel.ERROR);
       }
     ));
-
-    it('should return the good level after config changes', inject(
-      [NGXLogger],
-      (logger: NGXLogger) => {
-        logger.updateConfig({ level: NgxLoggerLevel.TRACE });
-
-        expect(logger.level).toEqual(NgxLoggerLevel.TRACE);
-      }
-    ));
   });
 
   describe('serverLogLevel', () => {
     it('should return the serverLogLevel', inject(
       [NGXLogger],
       (logger: NGXLogger) => {
-        expect(logger.serverLogLevel).toBe(undefined);
+        expect(logger.serverLogLevel).toBe(NgxLoggerLevel.OFF);
       }
     ));
-
-    it('should return the good serverLogLevel after config changes', inject(
-      [NGXLogger],
-      (logger: NGXLogger) => {
-        logger.updateConfig({ level: NgxLoggerLevel.ERROR, serverLogLevel: NgxLoggerLevel.TRACE });
-
-        expect(logger.serverLogLevel).toEqual(NgxLoggerLevel.TRACE);
-      }
-    ));
-  });
-
-  describe('setCustomHttpHeaders', () => {
-    // TODO
-  });
-
-  describe('setCustomParams', () => {
-    // TODO
-  });
-
-  describe('setWithCredentialsOptionValue', () => {
-    // TODO
   });
 
   describe('registerMonitor', () => {
@@ -216,80 +154,72 @@ describe('NGXLogger', () => {
     // TODO
   });
 
-  describe('_logIE', () => {
-    // TODO
-  });
-
-  describe('_logModern', () => {
-    // TODO
-  });
-
   describe('_log', () => {
-    // TODO
-  });
-});
+    it('should not do anything if nothing should be called', inject(
+      [NGXLogger, TOKEN_LOGGER_RULES_SERVICE, TOKEN_LOGGER_METADATA_SERVICE],
+      (logger: NGXLogger, ruleService: NGXLoggerRulesServiceMock, metadataService: NGXLoggerMetadataServiceMock) => {
+        spyOn(ruleService, 'shouldCallWritter').and.returnValue(false);
+        spyOn(ruleService, 'shouldCallServer').and.returnValue(false);
+        spyOn(ruleService, 'shouldCallMonitor').and.returnValue(false);
 
-describe('NGXLogger with mapper service', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-      ],
-      providers: [
-        NGXLogger,
-        NGXMapperService,
-        { provide: NGXLoggerHttpService, useClass: NGXLoggerHttpServiceMock },
-        { provide: LoggerConfig, useValue: { level: NgxLoggerLevel.ERROR } },
-        DatePipe
-      ]
-    });
-  });
+        const metadataSpy = spyOn(metadataService, 'getMetadata');
 
-  describe('call stack', () => {
-    it('should get good sourceMap', inject(
-      [NGXLogger],
-      (logger: NGXLogger) => {
+        logger.error('Test');
 
-        const mapperService = TestBed.inject(NGXMapperService);
-
-        const _getSourceMapSpy = spyOn<any>(mapperService, '_getSourceMap')
-          // calling fake _getSourceMap so it does not fail the test because of failed http request
-          .and.callFake((_: string, distPosition: LogPosition) => of(distPosition));
-
-        logger.updateConfig({
-          level: NgxLoggerLevel.ERROR,
-          enableSourceMaps: true,
-        });
-
-        logger.error('test should get good sourceMap');
-
-        // trying to match the sourceMap URL that looks like this : http://localhost:9876/src/lib/logger.service.spec.ts.map
-        expect(_getSourceMapSpy).toHaveBeenCalledWith(jasmine.stringMatching('\/src\/lib\/logger\.service\.spec\.ts\.map'), jasmine.anything());
+        expect(metadataSpy).not.toHaveBeenCalled();
       }
     ));
 
-    it('should get good LogPosition', inject(
-      [NGXLogger],
-      (logger: NGXLogger) => {
+    it('should not call writer', inject(
+      [NGXLogger, TOKEN_LOGGER_RULES_SERVICE, TOKEN_LOGGER_WRITER_SERVICE],
+      (logger: NGXLogger, ruleService: NGXLoggerRulesServiceMock, writerService: NGXLoggerWriterServiceMock) => {
+        spyOn(ruleService, 'shouldCallWritter').and.returnValue(false);
 
-        const mapperService = TestBed.inject(NGXMapperService);
+        const writerSpy = spyOn(writerService, 'writeMessage');
 
-        const _getSourceMapSpy = spyOn<any>(mapperService, '_getSourceMap')
-          // calling fake _getSourceMap so it does not fail the test because of failed http request
-          .and.callFake((_: string, distPosition: LogPosition) => of(distPosition));
+        logger.error('Test');
 
-        logger.updateConfig({
-          level: NgxLoggerLevel.ERROR,
-          enableSourceMaps: true,
-        });
+        expect(writerSpy).not.toHaveBeenCalled();
+      }
+    ));
 
-        logger.error('test should get good LogPosition');
+    it('should call writer', inject(
+      [NGXLogger, TOKEN_LOGGER_RULES_SERVICE, TOKEN_LOGGER_WRITER_SERVICE],
+      (logger: NGXLogger, ruleService: NGXLoggerRulesServiceMock, writerService: NGXLoggerWriterServiceMock) => {
+        spyOn(ruleService, 'shouldCallWritter').and.returnValue(true);
 
-        // trying to match the LogPosition that looks like this logger.service.spec.ts:lineNumber:columnNumber
-        // not trying to test the lineNumber and the columnNumber as these would change when the spec file changes and is too heavy to maintain
-        expect(_getSourceMapSpy).toHaveBeenCalledWith(jasmine.anything(), jasmine.objectContaining({ fileName: 'logger.service.spec.ts' }));
+        const writerSpy = spyOn(writerService, 'writeMessage');
+
+        logger.error('Test');
+
+        expect(writerSpy).toHaveBeenCalled();
+      }
+    ));
+
+    it('should not call server', inject(
+      [NGXLogger, TOKEN_LOGGER_RULES_SERVICE, TOKEN_LOGGER_SERVER_SERVICE],
+      (logger: NGXLogger, ruleService: NGXLoggerRulesServiceMock, serverService: NGXLoggerServerServiceMock) => {
+        spyOn(ruleService, 'shouldCallServer').and.returnValue(false);
+
+        const serverSpy = spyOn(serverService, 'sendToServer');
+
+        logger.error('Test');
+
+        expect(serverSpy).not.toHaveBeenCalled();
+      }
+    ));
+
+    it('should call server', inject(
+      [NGXLogger, TOKEN_LOGGER_RULES_SERVICE, TOKEN_LOGGER_SERVER_SERVICE],
+      (logger: NGXLogger, ruleService: NGXLoggerRulesServiceMock, serverService: NGXLoggerServerServiceMock) => {
+        spyOn(ruleService, 'shouldCallServer').and.returnValue(true);
+
+        const serverSpy = spyOn(serverService, 'sendToServer');
+
+        logger.error('Test');
+
+        expect(serverSpy).toHaveBeenCalled();
       }
     ));
   });
-
 });
