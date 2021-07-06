95397d6b4c5e4bc17647a257cea471614a7b0bf9
bmtheo <31310846+bmtheo@users.noreply.github.com>
2021-07-06 19:22:40 +0200

    Adds forchild test project
    this project helps me testing differences between forChild and forRoot calls

---
+++ projects/forchild/src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { NGXLogger } from "src/public_api";
+
+@Component({
+  templateUrl: './main.component.html',
+})
+export class MainComponent {
+  constructor(private logger: NGXLogger) {
+    
+  }
+
+  log(): void {
+    this.logger.debug('Test');
+  }
+}
